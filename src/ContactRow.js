e530cb0434ea588bd7e5ff739c664dd658dc4bcb
JayciPlamondon <142915613+JayciPlamondon@users.noreply.github.com>
2023-11-03 12:38:15 -0400

    Update ContactRow.js

--- src/ContactRow.js
+++ src/ContactRow.js
@@ -2,7 +2,7 @@
  * @param {*} props
  * @return {jsx}
  */
-export default function ContactRow({name, phone, email}) { // déconstruction de props
+export default function ContactRow({name, phone, email}) { // déconstruction de props (si on connait le nombre de props)
   return (
     <>
       <tr>
